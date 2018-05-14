89ee1f8051db0295032aa8587bca039431344276
Jan Gana <jan.gana@ipd.sk>
2018-05-14 06:36:47 +0200

    solution 3 updates

--- solution-3/js/scripts.js
+++ solution-3/js/scripts.js
@@ -30,6 +30,33 @@ fetch("https://gist.githubusercontent.com/paulccarey/cc04492efb89fe1400e814fa237
                                   ${myJson.recruiters[myJson.jobs[job].placed_by].name}
                                 </div>
                                 <span>${statusString.replace("position_", " ").toUpperCase()}</span>
+
+                              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#${myJson.jobs[job].placed_by}">
+                                More info
+                              </button>
+                              </div>
+
+                              <div class="modal fade" id="${myJson.jobs[job].placed_by}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                  <div class="modal-content">
+                                    <div class="modal-header">
+                                      <h5 class="modal-title" id="exampleModalLabel"><em>Job title:</em> ${myJson.jobs[job].title}</h5>
+                                    </div>
+                                    <div class="modal-body">
+                                      <div><em>Location:</em> ${myJson.jobs[job].location}</div>
+                                      <div><em>Salary:</em> ${myJson.jobs[job].salary}£</div>
+                                      <div><em>Status:</em> ${myJson.jobs[job].status.replace("position_", "").toUpperCase()}</div>
+                                      <div><em>Filled By:</em> ${myJson.recruiters[myJson.jobs[job].placed_by].name}</div>
+                                      <div><em>Recruiter's Live Jobs:</em> ${myJson.recruiters[myJson.jobs[job].placed_by].live_job_count}</div>
+                                      <div><em>Recruiter's Filled Jobs:</em> ${myJson.recruiters[myJson.jobs[job].placed_by].filled_job_count}</div>
+                                      <div><em>Recruiter Rating:</em> ${myJson.recruiters[myJson.jobs[job].placed_by].rating}</div>
+                                    </div>
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
                               </div>
                             `;
 
@@ -58,3 +85,8 @@ $(function() {
     $('.' + $(this).val()).show();
   });
 });
+
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
